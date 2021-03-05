b91f4b8071ea7ed131d5bf26f0f83836bf7fb59f
Daniil Bondarcov <1234567890bonddan@mail.ru>
2021-03-05 14:56:08 +0300

    fix

--- second_lab_/Lab2 task3/Program.cs
+++ second_lab_/Lab2 task3/Program.cs
@@ -16,14 +16,14 @@ namespace StrTask
 			for (ulong i = a; i <= b; ++i)
 			{
 				sieve.Add(i);
-			}
-			//	List<ulong> sieve = Enumerable.Range((int)a, (int)b - (int)a + 1).ToList();		
+			}		
 			for (ulong k = 2; k <= b; k *= 2)
 			{
-					amount += (ulong)(sieve.Count - sieve.RemoveAll(i => i % k != 0));				
+				amount += (ulong)(sieve.Count - sieve.RemoveAll(i => i % k != 0));				
 			}
 			Console.WriteLine("answer is 2 ^ {0} = {1}", amount,  Math.Pow(2, amount));
 		}
+		
 		static private bool isDivided(ulong number, ulong k)
 		{
 			return (number % k == 0) ? true : false;
