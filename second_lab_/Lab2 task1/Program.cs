11d9c7e2244a9ffe57bd9b1247c0a3dc21df49a3
Daniil Bondarcov <1234567890bonddan@mail.ru>
2021-03-06 20:27:27 +0300

    add checking for incorrect input

--- second_lab_/Lab2 task1/Program.cs
+++ second_lab_/Lab2 task1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace StrTask
 {
@@ -7,6 +7,10 @@ namespace StrTask
 		static void Main(string[] args)
 		{
 			string strNumber = Console.ReadLine();
+			while (!IsNumber(strNumber)) {
+				Console.WriteLine("Incorrect input, enter valid data!");
+				strNumber = Console.ReadLine();
+			}
 			string beforeDot = "", afterDot = "";
 			int indexOfDot = strNumber.IndexOf('.');
 			if (indexOfDot == -1) indexOfDot = strNumber.Length;
@@ -16,22 +20,37 @@ namespace StrTask
 			for (int i = startIndex; i < indexOfDot; ++i) {
 				beforeDot += strNumber[i];
 			}
-			for (int i = indexOfDot + 1; i < strNumber.Length; ++i)
-			{
+			for (int i = indexOfDot + 1; i < strNumber.Length; ++i) {
 				afterDot += strNumber[i];
 			}
 			double answer = FromStringToInt(beforeDot) + FromStringToInt(afterDot) / Math.Pow(10, afterDot.Length);
 			if (isNegative) answer *= -1;
 			Console.WriteLine(answer);
 		}
-		static private int FromStringToInt(string strNumber) {
+		static private int FromStringToInt(string strNumber)
+		{
 			int result = 0;
 			int k = (int)Math.Pow(10, strNumber.Length - 1);
-			for (int i = 0; i < strNumber.Length; ++i){
+			for (int i = 0; i < strNumber.Length; ++i) {
 				result += k * ((int)strNumber[i] - (int)'0');
 				k /= 10;
 			}
-			return result;	
+			return result;
+		}
+		static private bool IsNumber(string strNumber) {
+			int numOfMinuses = 0, numOfDots = 0;
+			foreach (char i in strNumber) {
+				if (char.IsDigit(i)) { }
+				else if (i == '.') ++numOfDots;
+				else if (i == '-') ++numOfMinuses;
+				else return false;
+			}
+			if (numOfDots <= 1 && numOfMinuses <= 1) {
+				if (numOfMinuses == 1 && strNumber[0] != '-')
+					return false;
+					else return true;
+			}
+			return false;
 		}
 	}
 }
