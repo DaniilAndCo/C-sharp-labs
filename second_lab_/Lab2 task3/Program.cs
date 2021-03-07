468c46c898f5b82c797d98d45f120e6a5dab96b5
Daniil Bondarcov <1234567890bonddan@mail.ru>
2021-03-07 10:10:18 +0300

    change algorithm

--- second_lab_/Lab2 task3/Program.cs
+++ second_lab_/Lab2 task3/Program.cs
@@ -1,29 +1,25 @@
 using System;
-using System.Collections.Generic;
+
 namespace rangeTask
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{	
-			ulong a, b, amount = 0;
-			a = ulong.Parse(Console.ReadLine());
-			b = ulong.Parse(Console.ReadLine());
-			List<ulong> sieve = new List<ulong>();
-			for (ulong i = a; i <= b; ++i)
-			{
-				sieve.Add(i);
-			}		
-			for (ulong k = 2; k <= b; k *= 2)
-			{
-				amount += (ulong)(sieve.Count - sieve.RemoveAll(i => i % k != 0));				
-			}
-			Console.WriteLine("answer is 2 ^ {0} = {1}", amount,  Math.Pow(2, amount));
-		}
-		
-		static private bool isDivided(ulong number, ulong k)
-		{
-			return (number % k == 0) ? true : false;
-		}
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int64 a = Int64.Parse(Console.ReadLine());
+            Int64 b = Int64.Parse(Console.ReadLine());
+            Int64 result = Algorithm(b) - Algorithm(a - 1);
+            Console.WriteLine("2 ^ {0} = {1}", result, Math.Pow(2, result));
+        }
+
+        static private Int64 Algorithm(Int64 number)
+        {
+            Int64 k = 2, result = 0;
+            for (int i = 1; i < 64; i++) {
+                result += number / k;
+                k *= 2;
+            }
+            return result;
+        }
+    }
 }
