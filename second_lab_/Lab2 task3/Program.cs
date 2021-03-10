3b3b75a8292a72def325eb2d0a9ad21288775521
Daniil Bondarcov <1234567890bonddan@mail.ru>
2021-03-10 23:51:06 +0300

    Update Program.cs

--- second_lab_/Lab2 task3/Program.cs
+++ second_lab_/Lab2 task3/Program.cs
@@ -6,16 +6,23 @@ namespace rangeTask
     {
         static void Main(string[] args)
         {
-            Int64 a = Int64.Parse(Console.ReadLine());
-            Int64 b = Int64.Parse(Console.ReadLine());
+            Int64 a, b;
+            while (!Int64.TryParse(Console.ReadLine(), out a))
+            {
+				Console.WriteLine("Enter valid data!");
+            }
+            while (!Int64.TryParse(Console.ReadLine(), out b))
+            {
+                Console.WriteLine("Enter valid data!");
+            }
             Int64 result = Algorithm(b) - Algorithm(a - 1);
-            Console.WriteLine("2 ^ {0} = {1}", result, Math.Pow(2, result));
+            Console.WriteLine("result => 2 ^ {0}", result);
         }
 
         static private Int64 Algorithm(Int64 number)
         {
             Int64 k = 2, result = 0;
-            for (int i = 1; i < 64; i++) {
+            for (int i = 1; i < 64; ++i) {
                 result += number / k;
                 k *= 2;
             }
